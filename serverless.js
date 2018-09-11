2c189cd267607c26ba16d10897d695394fae437d
lulzneko <lulzneko@riotz.works>
2018-09-11 13:20:54 +0900

    🔧 Configure basic settings of Serverless to deploy application

---
+++ serverless.js
@@ -0,0 +1,29 @@
+// Command line options
+// --stage:       Required System Landscape name, default is 'dev' (Choice: [dev | prd], e.g. --stage dev)
+// --region:      Optional, default is determined by the value of `stage` (e.g. --region ap-northeast-1)
+// --bucket:      Optional, default is determined by the value of `stage` (e.g. --bucket x-sls-artifacts)
+// --aws-profile: Optional, when specifying AWS Profile name (If it exists in `~/.aws/credentials`, e.g. --aws-profile dev )
+
+const pkg = require('./package.json');
+
+
+module.exports = {
+  service: pkg.name,
+  provider: {
+    name: 'aws',
+    stage: '${opt:stage, "dev"}',
+    region: '${opt:region, self:custom.regions.${self:provider.stage}}',
+    runtime: `nodejs${pkg.engines.node}`,
+    memorySize: 512,
+    timeout: 29,
+    deploymentBucket: {
+      name: '${opt:bucket, "x-sls-artifacts-${self:service}-${self:provider.region}"}',
+      serverSideEncryption: 'AES256'
+    }
+  },
+
+  custom: {
+    regions:  { dev: 'us-east-1', prd: 'us-west-2' },
+    suffixes: { dev: '-dev',      prd: '' }
+  }
+};
