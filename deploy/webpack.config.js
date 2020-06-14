54c8a6a1da1dd01c48b461e01150fc7bdf43d8c6
lulzneko <lulzneko@riotz.works>
2020-06-14 11:44:07 +0900

    Refactoring serverless build configuration

---
+++ deploy/webpack.config.js
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/camelcase */   // 'cuz key name of configuration is specified
+
+const path = require('path');
+const slsw = require('serverless-webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const externals = require('webpack-node-externals');
+
+const production = slsw.lib.options.stage === 'prd';
+const entries = production ? slsw.lib.entries : [ ...new Map(Object.entries(slsw.lib.entries)) ].reduce((l, [ k, v ]) => Object.assign(l, { [k]: [ './deploy/source-map-install.js', v ]}), {});
+
+
+module.exports = {
+  mode: production ? 'production' : 'development',
+  target: 'async-node',
+  entry: entries,
+  devtool: production ? '' : 'inline-source-map',
+  externals: [
+    externals({
+      modulesFromFile: {
+        include: [ 'dependencies', ...production ? [] : [ 'source-map-support' ] ]
+      }
+    })
+  ],
+  resolve: {
+    extensions: [ '.js', '.jsx', '.json', '.ts', '.tsx' ],
+    alias: {
+      '~': path.join(__dirname, '../src')
+    }
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/u, use: 'ts-loader' }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: production
+          }
+        }
+      })
+    ]
+  },
+  output: {
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+    filename: '[name].js'
+  }
+};
--- serverless.js
+++ serverless.js
@@ -1,8 +1,11 @@
+/* eslint-disable no-multi-spaces */              // 'cuz clarify paired structure
+/* eslint-disable no-template-curly-in-string */  // 'cuz syntax of the serverless framework
+
 // Command line options
 // --stage:       Required System Landscape name, default is 'dev' (Choice: [dev | prd], e.g. --stage dev)
 // --region:      Optional, default is determined by the value of `stage` (e.g. --region ap-northeast-1)
 // --bucket:      Optional, default is determined by the value of `stage` (e.g. --bucket x-sls-artifacts)
-// --aws-profile: Optional, when specifying AWS Profile name (If it exists in `~/.aws/credentials`, e.g. --aws-profile dev )
+// --aws-profile: Optional, when specifying AWS Profile name (If `dev` exists in `~/.aws/credentials`, e.g. --aws-profile dev )
 
 const pkg = require('./package.json');
 
@@ -12,24 +15,23 @@ module.exports = {
   provider: {
     name: 'aws',
     stage: '${opt:stage, "dev"}',
-    region: '${opt:region, self:custom.regions.${self:provider.stage}}',
+    region: '${opt:region, self:custom.stages.region.${self:provider.stage}}',
     runtime: `nodejs${pkg.engines.node}`,
-    memorySize: 512,
+    memorySize: 256,
     timeout: 29,
-    logRetentionInDays: 30,
+    logRetentionInDays: 7,
     deploymentBucket: {
       name: '${opt:bucket, "x-sls-artifacts-riotz-${self:provider.region}"}',
       serverSideEncryption: 'AES256'
     },
-    iamRoleStatements: [
-      {
-        Effect: 'Allow',
-        Action: [
-          'dynamodb:*',
-        ],
-        Resource: '*',
-      },
-    ],
+    apiName: '${self:service}${self:custom.stages.suffix.${self:provider.stage}}',
+    iamRoleStatements: [{
+      Effect: 'Allow',
+      Action: [
+        'dynamodb:*'
+      ],
+      Resource: '*'
+    }],
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: 1
     }
@@ -37,20 +39,23 @@ module.exports = {
 
   plugins: [
     'serverless-webpack',
+    'serverless-offline',
     'serverless-dynamodb-ttl'
   ],
 
   custom: {
-    webpack: { packager: 'yarn', includeModules: { forceExclude: [ 'aws-sdk' ]}},
-    regions:  { dev: 'ap-northeast-1', prd: '' },
-    suffixes: { dev: '',               prd: '' },
+    webpack: { packager: 'yarn', webpackConfig: './deploy/webpack.config.js', includeModules: { forceExclude: [ 'aws-sdk' ]}},
+    stages: {
+      region: { dev: 'ap-northeast-1', prd: '' },
+      suffix: { dev: '-dev',           prd: '' }
+    },
     names: {
-      'lambda-systems': '${self:service}-systems${self:custom.suffixes.${self:provider.stage}}',
-      'lambda-post-rooms': '${self:service}-post-rooms${self:custom.suffixes.${self:provider.stage}}',
-      'lambda-get-rooms': '${self:service}-get-rooms${self:custom.suffixes.${self:provider.stage}}',
-      'lambda-get-rooms-roomId': '${self:service}-get-rooms-roomId${self:custom.suffixes.${self:provider.stage}}',
-      'lambda-post-rooms-roomId': '${self:service}-post-rooms-roomId${self:custom.suffixes.${self:provider.stage}}',
-      'lambda-get-rooms-match': '${self:service}-get-rooms-match${self:custom.suffixes.${self:provider.stage}}',
+      'lambda-systems': '${self:service}-systems${self:custom.stages.suffix.${self:provider.stage}}',
+      'lambda-post-rooms': '${self:service}-post-rooms${self:custom.stages.suffix.${self:provider.stage}}',
+      'lambda-get-rooms': '${self:service}-get-rooms${self:custom.stages.suffix.${self:provider.stage}}',
+      'lambda-get-rooms-roomId': '${self:service}-get-rooms-roomId${self:custom.stages.suffix.${self:provider.stage}}',
+      'lambda-post-rooms-roomId': '${self:service}-post-rooms-roomId${self:custom.stages.suffix.${self:provider.stage}}',
+      'lambda-get-rooms-match': '${self:service}-get-rooms-match${self:custom.stages.suffix.${self:provider.stage}}'
     },
     dynamodb: {
       ttl: [
@@ -72,44 +77,44 @@ module.exports = {
     GetRooms: {
       name: '${self:custom.names.lambda-get-rooms}',
       handler: 'src/aws-lambda-handler/list-rooms.handle',
-      events: [{ http: { path: 'rooms', method: 'get', cors: true }}],
+      events: [{ http: { path: 'rooms', method: 'get', cors: true }}]
     },
     PostRooms: {
       name: '${self:custom.names.lambda-post-rooms}',
       handler: 'src/aws-lambda-handler/new-room.handle',
-      events: [{ http: { path: 'rooms', method: 'post', cors: true }}],
+      events: [{ http: { path: 'rooms', method: 'post', cors: true }}]
     },
     GetRoomsRoomId: {
       name: '${self:custom.names.lambda-get-rooms-roomId}',
       handler: 'src/aws-lambda-handler/get-room.handle',
-      events: [{ http: { path: 'rooms/{roomId}', method: 'get', cors: true }}],
+      events: [{ http: { path: 'rooms/{roomId}', method: 'get', cors: true }}]
     },
     PostRoomsRoomId: {
       name: '${self:custom.names.lambda-post-rooms-roomId}',
       handler: 'src/aws-lambda-handler/add-rapper.handle',
-      events: [{ http: { path: 'rooms/{roomId}', method: 'post', cors: true }}],
+      events: [{ http: { path: 'rooms/{roomId}', method: 'post', cors: true }}]
     },
     GetRoomsRoomIdMatch: {
       name: '${self:custom.names.lambda-get-rooms-match}',
       handler: 'src/aws-lambda-handler/list-match-rooms.handle',
-      events: [{ http: { path: 'rooms/match', method: 'get', cors: true }}],
-    },
+      events: [{ http: { path: 'rooms/match', method: 'get', cors: true }}]
+    }
   },
 
-  resources: {
+  resources: [{
     Resources: {
       RoomsTable: {
         Type: 'AWS::DynamoDB::Table',
         Properties: {
           TableName: 'Rooms',
           AttributeDefinitions: [
-            {AttributeName: 'registeredDateYearMonth', AttributeType: 'S'},
-            {AttributeName: 'registeredDateRoomId', AttributeType: 'S'},
-            {AttributeName: 'roomId', AttributeType: 'S'},
+            { AttributeName: 'registeredDateYearMonth', AttributeType: 'S' },
+            { AttributeName: 'registeredDateRoomId', AttributeType: 'S' },
+            { AttributeName: 'roomId', AttributeType: 'S' }
           ],
           KeySchema: [
-            {AttributeName: 'registeredDateYearMonth', KeyType: 'HASH'},
-            {AttributeName: 'registeredDateRoomId', KeyType: 'RANGE'},
+            { AttributeName: 'registeredDateYearMonth', KeyType: 'HASH' },
+            { AttributeName: 'registeredDateRoomId', KeyType: 'RANGE' }
           ],
           ProvisionedThroughput: {
             ReadCapacityUnits: 1,
@@ -119,7 +124,7 @@ module.exports = {
             IndexName: 'roomId-index',
             KeySchema: [
               { AttributeName: 'roomId', KeyType: 'HASH' },
-              { AttributeName: 'registeredDateRoomId', KeyType: 'RANGE' },
+              { AttributeName: 'registeredDateRoomId', KeyType: 'RANGE' }
             ],
             Projection: {
               ProjectionType: 'ALL'
@@ -127,10 +132,10 @@ module.exports = {
             ProvisionedThroughput: {
               ReadCapacityUnits: 1,
               WriteCapacityUnits: 1
-            },
+            }
           }]
         }
       }
     }
-  }
+  }]
 };
